60e33e56d7f2a117e1f6aad30d0ccb4017a2d803
Raj Kumar <rajkmr.rkp@gmail.com>
2020-10-19 15:48:20 +0530

    Updates

---
+++ precache-manifest.9db42aa654bf5bffbfd2322c65349caa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37d27b13409bccaf7bb7c5c533a94a1f",
+    "url": "/product-customiser-react/index.html"
+  },
+  {
+    "revision": "c0f7ead5d09baa4ab465",
+    "url": "/product-customiser-react/static/css/main.1d44f3a8.chunk.css"
+  },
+  {
+    "revision": "8a148d5abc3359bd281e",
+    "url": "/product-customiser-react/static/js/2.f4c8fd08.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/product-customiser-react/static/js/2.f4c8fd08.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c0f7ead5d09baa4ab465",
+    "url": "/product-customiser-react/static/js/main.a5a21d07.chunk.js"
+  },
+  {
+    "revision": "04a06fcd14050103308c",
+    "url": "/product-customiser-react/static/js/runtime-main.fcf8b4d1.js"
+  }
+]);
